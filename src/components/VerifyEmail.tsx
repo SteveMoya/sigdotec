96ddd27e06721d5d7e275c1c3846e6e5515bd3ee
Steve Moya Cepeda <100615052@est.uasd.edu.do>
2024-06-28 14:41:44 -1200

    Update file references and dependencies

--- src/components/VerifyEmail.tsx
+++ src/components/VerifyEmail.tsx
@@ -1,4 +1,4 @@
-"use server"
+ 
 import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
