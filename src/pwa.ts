fc909649f89d9606c0fa2dd5cf3c0ff92adf849a
Steve Moya Cepeda <100615052@est.uasd.edu.do>
2024-07-26 05:01:41 -1200

    Update PWA registration in pwa.ts

--- src/pwa.ts
+++ src/pwa.ts
@@ -1,5 +1,2 @@
-// // import { registerSW } from "virtual:pwa-register"; // registerSW({ //
-// immediate: true, // onRegisteredSW(swScriptUrl) { // // eslint-disable-next-line
-// no-console // console.log("SW registered: ", swScriptUrl); // }, //
-// onOfflineReady() { // // eslint-disable-next-line no-console // console.log("PWA
-// application ready to work offline"); // }, // });
+// import { registerSW } from "virtual:pwa-register"; 
+//  registerSW({  immediate: true, onRegisteredSW: (swScriptUrl) => { console.log("SW registered: ", swScriptUrl); }, onOfflineReady: () => { console.log("PWA application ready to work offline"); } });
