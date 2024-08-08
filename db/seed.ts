d7da4f45ef28be0d442ed24d900ed42c7856434e
Steve Moya Cepeda <100615052@est.uasd.edu.do>
2024-08-08 11:49:44 -1200

    feat: Agregar datos demográficos y transacciones de billetera a la función de semillas
    This commit adds demographic data and wallet transactions to the seed function. It generates dummy data for users' demographics and wallet transactions, including birthdates, genders, provinces, working places, subjects, amounts, types, and creation dates. This will help populate the database with realistic data for testing and development purposes.

--- db/seed.ts
+++ db/seed.ts
@@ -1,8 +1,26 @@
+
 import { hasherPassword } from '@/utils';
-import { User, db, Demographic } from 'astro:db';
+import { User, db, Demography, WalletTransaction } from 'astro:db';
+import { create } from 'domain';
 import { generateId } from 'lucia';
 
 // https://astro.build/db/seed
+
+function generateCreatedAt() {
+	const date = new Date();
+	date.setMonth(date.getMonth() - 1);
+	return date;
+}
+function generateCreatedAt2() {
+	const date = new Date();
+	date.setMonth(date.getMonth() - 2);
+	return date;
+}
+function generateCreateAtThisMonth() {
+	const date = new Date();
+	date.setMonth(date.getMonth());
+	return date;
+}
 
 export default async function seed() {
 	const seedsUsers = [
@@ -60,26 +78,130 @@ export default async function seed() {
 			balance: 1000,
 		},
 	]
-	// const DemographycUsers = [
-	// 	{
-	// 		id: generateId(15),
-	// 		userId: seedsUsers[0].id,
-	// 		age: 25,
-	// 		gender: "Masculino",
-	// 		province: "Santo Domingo",
-	// 		workingPlace: "Universidad Autonoma de Santo Domingo",
-	// 		subject: "Matemática Secundaria",
-	// 	},
-	// 	{
-	// 		id: generateId(15),
-	// 		userId: seedsUsers[1].id,
-	// 		age: 30,
-	// 		gender: "Masculino",
-	// 		province: "Azua",
-	// 		workingPlace: "Universidad Autonoma de Santo Domingo",
-	// 		subject: "Matemática Primaria",
-	// 	},
-	// ]
+	const DemographycUsers = [
+		{
+			id: generateId(15),
+			userId: seedsUsers[0].id,
+			birthdate: new Date(1999, 1, 1),
+			gender: "Masculino",
+			province: "Santo Domingo",
+			workingPlace: "Universidad Autonoma de Santo Domingo",
+			subject: "Matemática Secundaria",
+		},
+		{
+			id: generateId(15),
+			userId: seedsUsers[1].id,
+			birthdate: new Date(1999, 1, 1),
+			gender: "Masculino",
+			province: "Azua",
+			workingPlace: "Universidad Autonoma de Santo Domingo",
+			subject: "Matemática Primaria",
+		},
+		{
+			id: generateId(15),
+			userId: seedsUsers[2].id,
+			birthdate: new Date(2002, 1, 1),
+			gender: "Femenino",
+			province: "Santiago",
+			workingPlace: "Universidad Autonoma de Santo Domingo",
+			subject: "Matemática Secundaria",
+		},
+		{
+			id: generateId(15),
+			userId: seedsUsers[3].id,
+			birthdate: new Date(2003, 1, 1),
+			gender: "Femenino",
+			province: "Santiago",
+			workingPlace: "Universidad Autonoma de Santo Domingo",
+			subject: "Matemática Secundaria",
+		},
+		{
+			id: generateId(15),
+			userId: seedsUsers[4].id,
+			birthdate: new Date(2003, 2, 1),
+			gender: "Masculino",
+			province: "Santo Domingo",
+			workingPlace: "Universidad Autonoma de Santo Domingo",
+			subject: "Matemática Secundaria",
+		},
+		{
+			id: generateId(15),
+			userId: seedsUsers[4].id,
+			birthdate: new Date(2003, 2, 1),
+			gender: "Masculino",
+			province: "Santo Domingo",
+			workingPlace: "Universidad Autonoma de Santo Domingo",
+			subject: "Matemática Secundaria",
+		}
+	]
+	const WalletTransactionSeeds = [
+		{
+			id: generateId(15),
+			transactionid: generateId(15),
+			userId: seedsUsers[0].id,
+			amount: 100,
+			type: "Paypal",
+			createAt: generateCreatedAt(),
+		},
+		{
+			id: generateId(15),
+			transactionid: generateId(15),
+			userId: seedsUsers[1].id,
+			amount: 10,
+			type: "ACH",
+			createAt: new Date(),
+		},
+		{
+			id: generateId(15),
+			transactionid: generateId(15),
+			userId: seedsUsers[2].id,
+			amount: 1500,
+			type: "ACH",
+			createAt: generateCreatedAt(),
+		},
+		{
+			id: generateId(15),
+			transactionid: generateId(15),
+			userId: seedsUsers[0].id,
+			amount: 500,
+			type: "ACH",
+			createAt: generateCreatedAt2(),
+		},
+		{
+			id: generateId(15),
+			transactionid: generateId(15),
+			userId: seedsUsers[4].id,
+			amount: 550,
+			type: "ACH",
+			createAt: generateCreatedAt2(),
+		},
+		{
+			id: generateId(15),
+			transactionid: generateId(15),
+			userId: seedsUsers[3].id,
+			amount: 300,
+			type: "ACH",
+			createAt: generateCreatedAt(),
+		},
+		{
+			id: generateId(15),
+			transactionid: generateId(15),
+			userId: seedsUsers[1].id,
+			amount: 350,
+			type: "Paypal",
+			createAt: generateCreateAtThisMonth(),
+		},
+		{
+			id: generateId(15),
+			transactionid: generateId(15),
+			userId: seedsUsers[1].id,
+			amount: 10,
+			type: "Paypal",
+			createAt: generateCreateAtThisMonth(),
+		}
+	]
+
 	await db.insert(User).values(seedsUsers)
-	// await db.insert(Demography).values(DemographyUsers)
+	await db.insert(Demography).values(DemographycUsers)
+	await db.insert(WalletTransaction).values(WalletTransactionSeeds)
 }
